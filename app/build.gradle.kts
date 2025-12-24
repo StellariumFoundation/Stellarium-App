2ca4822cb960f5a375694969c87d759db405fcd9
StellariumFoundation <stellar.foundation.us@gmail.com>
2025-12-24 12:59:49 -0300

    Last Sync: 2025-12-24 12:59 (Mobile)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jv.stellariumapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 1.1
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
